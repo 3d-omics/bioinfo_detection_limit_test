a14fc5b4dc3a90660b06bf4b28a2e677603576fd
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2023-07-18 15:11:23 +0200

    filter out empty nonpareil files

--- workflow/scripts/aggregate_nonpareil.R
+++ workflow/scripts/aggregate_nonpareil.R
@@ -27,9 +27,15 @@ output_folder <- dirname(output_file)
 
 dir.create(output_folder, showWarnings = FALSE, recursive = TRUE)
 
-files <- list.files(args$input_folder, pattern = "*.npo", full.names = TRUE)
+nonempty_files <-
+  list.files(args$input_folder, pattern = "*.npo", full.names = TRUE) %>%
+  data.frame(file = .) %>%
+  mutate(size = file.info(file)$size) %>%
+  filter(size > 0) %>%
+  pull(file)
 
-Nonpareil.set(files, plot = F) %>%
+nonempty_files %>%
+  Nonpareil.set(plot = F) %>%
   summary() %>%
   as.data.frame() %>%
   rownames_to_column("sample_id") %>%
