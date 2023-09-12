2d7a9779ffd085b66ca36e1cb0ce88c80c80dad4
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2023-09-12 16:26:21 +0200

    Fix pylint issues

--- workflow/scripts/metagenome_pipeline.py
+++ workflow/scripts/metagenome_pipeline.py
@@ -42,56 +42,59 @@ import pandas as pd
 
 
 def remove_path_from_columns_names(data_frame):
+    """remove folder names and extension from the column names"""
     list_of_columns = data_frame.columns.tolist()
     simplified_columns = []
     for file_name in list_of_columns:
-        if file_name.find("/") != -1:  # Remove path from the file name is present
-            file_name = file_name.split("/")[-1]
-            file_name = file_name.split(".")[0]
-        else:
-            file_name = file_name.split(".")[0]
+        file_name = file_name.split("/")[-1]
+        file_name = file_name.split(".")[0]
         if file_name[0].isdigit():  # Add letter to the file name is starts from digit
             file_name = "f_" + file_name
-        if file_name.find("-") != -1:  # Replace '-' to '_' if present
-            file_name = file_name.replace("-", "_")
+        file_name = file_name.replace("-", "_")
         simplified_columns.append(file_name)
-    return_df = data_frame.copy()
-    return_df.columns = simplified_columns
-    return return_df
+    data_frame.columns = simplified_columns
+    return data_frame
 
 
-# =====================================================
+if __name__ == "__main__":
+    READS_TABLE_PATH = sys.argv[1]
+    COVERAGE_TABLE_PATH = sys.argv[2]
+    DATASET_NAME = sys.argv[3]
 
-reads_table_path = sys.argv[1]
-coverage_table_path = sys.argv[2]
-dataset_name = sys.argv[3]
+    reads_df = pd.read_csv(READS_TABLE_PATH, sep="\t")
+    coverage_df = pd.read_csv(COVERAGE_TABLE_PATH, sep="\t")
 
-reads_df = pd.read_csv(reads_table_path, sep="\t")
-coverage_df = pd.read_csv(coverage_table_path, sep="\t")
-# Simplify column names
-reads_counts_df = remove_path_from_columns_names(reads_df)
-coverage_counts_df = remove_path_from_columns_names(coverage_df)
-# Remove coordinates columns
-reads_counts_df = reads_counts_df.drop(columns=["start", "stop"])
-coverage_counts_df = coverage_counts_df.drop(columns=["start", "stop"])
-# Aggregate species
-reads_counts_df.loc[:, "specie"] = reads_counts_df["chrom"].str.split("_").str[0]
-coverage_counts_df.loc[:, "specie"] = coverage_counts_df["chrom"].str.split("_").str[0]
-reads_counts_sum = reads_counts_df.groupby(["specie"]).sum()
-coverage_counts_sum = coverage_counts_df.groupby(["specie"]).sum()
-# Calculate percentage for reads
-total_reads = reads_counts_sum.sum()
-reads_counts_percent = reads_counts_sum.div(total_reads)
-reads_counts_percent = reads_counts_percent * 100
-# Save output tables
-reads_counts_sum.to_csv(
-    str(date.today()) + "." + dataset_name + ".reads.counts.by.organism.table", sep="\t"
-)
-reads_counts_percent.to_csv(
-    str(date.today()) + "." + dataset_name + ".reads.percent.by.organism.table",
-    sep="\t",
-)
-coverage_counts_sum.to_csv(
-    str(date.today()) + "." + dataset_name + ".coverage.counts.by.organism.table",
-    sep="\t",
-)
+    # Simplify column names
+    reads_counts_df = remove_path_from_columns_names(reads_df)
+    coverage_counts_df = remove_path_from_columns_names(coverage_df)
+
+    # Remove coordinates columns
+    reads_counts_df = reads_counts_df.drop(columns=["start", "stop"])
+    coverage_counts_df = coverage_counts_df.drop(columns=["start", "stop"])
+
+    # Aggregate species
+    reads_counts_df.loc[:, "specie"] = reads_counts_df["chrom"].str.split("_").str[0]
+    coverage_counts_df.loc[:, "specie"] = (
+        coverage_counts_df["chrom"].str.split("_").str[0]
+    )
+    reads_counts_sum = reads_counts_df.groupby(["specie"]).sum()
+    coverage_counts_sum = coverage_counts_df.groupby(["specie"]).sum()
+
+    # Calculate percentage for reads
+    total_reads = reads_counts_sum.sum()
+    reads_counts_percent = reads_counts_sum.div(total_reads)
+    reads_counts_percent = reads_counts_percent * 100
+
+    # Save output tables
+    reads_counts_sum.to_csv(
+        str(date.today()) + "." + DATASET_NAME + ".reads.counts.by.organism.table",
+        sep="\t",
+    )
+    reads_counts_percent.to_csv(
+        str(date.today()) + "." + DATASET_NAME + ".reads.percent.by.organism.table",
+        sep="\t",
+    )
+    coverage_counts_sum.to_csv(
+        str(date.today()) + "." + DATASET_NAME + ".coverage.counts.by.organism.table",
+        sep="\t",
+    )
